4dc07e58c161fc5e9577d011642b4ea3f132d53b
Rodela Ghosh <ghoshrodela111@gmail.com>
2025-05-29 17:34:27 -0400

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,8 @@
+import globe from '../images/globe.png'
 export function Header(){
     return(
         <header>
-            <img className="logo-img" alt="globe icon" src="./images/globe.png"/>
+            <img className="logo-img" alt="globe icon" src={globe}/>
             <span>my travel journal.</span>
         </header>
     )
